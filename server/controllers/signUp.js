fff4c4d3288367bfe0f18da5f715358accb48ad5
lazytech614 <derupanjan2021@gmail.com>
2024-11-02 23:58:38 +0530

    create sign up api

---
+++ server/controllers/signUp.js
@@ -0,0 +1,42 @@
+import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
+import { client } from "../db/connection.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
+export const signUp = async (req, res) => {
+  const { name, username, email, password, confirmPassword } = req.body;
+
+  if (password !== confirmPassword)
+    return res.status(400).json({ message: "Passwords do not match" });
+
+  try {
+    const userResult = await client.query(
+      "SELECT * FROM users WHERE username = $1 OR email = $2",
+      [username, email]
+    );
+
+    if (userResult.rows.length > 0)
+      return res.status(400).json({ message: "User already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUserResult = await client.query(
+      `INSERT INTO users (name, username, password, email) 
+         VALUES ($1, $2, $3, $4) RETURNING *`,
+      [name, username, hashedPassword, email]
+    );
+
+    console.log(newUserResult.rows[0]);
+    const newUser = newUserResult.rows[0];
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser.username, res);
+      res.status(201).json({ message: "New user created successfully" });
+    } else {
+      res.status(400).json({ message: "Invalid data" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { signIn } from "../controllers/signIn.js";
 import { logOut } from "../controllers/LogOut.js";
+import { signUp } from "../controllers/signUp.js";
 
 const router = Router();
 
 router.post("/signin", signIn);
-router.post("/signup");
+router.post("/signup", signUp);
 router.post("/logout", logOut);
 
 export default router;
