d71a7c8e119ba97c7655959f201f1c5c230651d3
lazytech614 <derupanjan2021@gmail.com>
2024-11-06 06:21:19 +0530

    clean up

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -1,22 +1,21 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
 import likeIcon from "/thumb-up-line.svg"
 import likeIconFill from "/thumb-up-fill.svg"
 import dislikeIcon from "/thumb-down-line.svg"
 import dislikeIconFill from "/thumb-down-fill.svg"
 import DOMPurify from 'dompurify';
-import toast from 'react-hot-toast'
 import useCheckReaction from '../hooks/useCheckReaction'
 import { useAuthContext } from '../context/AuthContext'
+import useReactClick from '../hooks/useReactClick'
 
 const BlogCard = ({ id,title, category, post, image }) => {
-  const [likesCount, setLikesCount] = useState(0);
-  const [dislikesCount, setDislikesCount] = useState(0);
   const navigate = useNavigate();
   const cleanHtml = DOMPurify.sanitize(post);
 
   const {status, checkReaction} = useCheckReaction(id)
   const {authUser} = useAuthContext()
+  const {react, likesCount, dislikesCount, setLikesCount, setDislikesCount} = useReactClick()
 
   const handleReadMore = () => {
     navigate('/blog-details', {
@@ -24,64 +23,13 @@ const BlogCard = ({ id,title, category, post, image }) => {
     });
   };
 
-  const fetchEngagementCounts = async () => {
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/api/blogs/get-blog-engagements/${id}`,
-        {   
-            credentials: "include",
-            method: "GET",
-            headers: { "Content-Type": "application/json" }
-        }
-      ).then((res) => res.json());
-
-      if (response.success) {
-        setLikesCount(response.data.like_count);
-        setDislikesCount(response.data.dislike_count);
-      } else {
-        console.error(response.message);
-      }
-    } catch (error) {
-      console.error("Error fetching engagement counts:", error);
-    }
-  };
-
   const handleReactClick = async (isLike) => {
-    const userId = JSON.parse(localStorage.getItem("userDetails"))?.userId || null;
-    if (!userId) {
-      toast.error("Please login to react");
-      return;
-    }
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/api/blogs/react-blog/${userId}/${id}`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ isLike })
-        }
-      ).then((res) => res.json());
-
-      if (response.success) {
-        toast.success(response.message);
-        isLike ? setLikesCount(likesCount + 1) : setDislikesCount(dislikesCount + 1);
-      } else {
-        console.error(response.message); 
-        toast.error(response.message);
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      toast.error(error.message);
-    }
+    await react(status,isLike, id)
   };
 
   useEffect(() => {
-    if (authUser) {
       checkReaction(id);
-    }
-    // console.log(status);
-    fetchEngagementCounts(id)
-  }, [])
+  }, [likesCount, dislikesCount, authUser, checkReaction, id]);
 
   return (
     <div className='border border-black rounded-md overflow-hidden flex flex-col h-full sm:hover:shadow-[-5px_5px_10px_#000000] duration-150'>
